26c5958b765e2df40bed4d4cca92baf1e104e597
rakesh.vasal <rakehvasal@gmail.com>
2016-08-24 12:05:58 +0530

    sadas

--- MainActivity.java
+++ MainActivity.java
@@ -9,9 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hello
-        //branch3
-        //branch3
+        //hello\
 
     }
 }
